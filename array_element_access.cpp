104c5578788a2af7d98c56035fa5b5cf8c7a63b8
Avi Sharma <avisharma5016@gmail.com>
2025-07-30 11:31:20 +0530

    array_element_access.cpp

---
+++ array_element_access.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[5];
+    cout<<"Enter 5 elements:";
+    for(int i=0; i<5; i++){
+        cin >> arr[i];
+    }
+    
+    cout<<"Enter element to be accessed: ";
+    int element;
+    cin>>element;
+    bool found;
+    for(int i=0; i<5; i++){
+        if(arr[i] == element){
+            cout<<"Element found at index: "<<i+1<<endl;
+            found = true;
+            break;
+        }
+    }
+    if (found == false){
+        cout<<"Element not found"<<endl;
+    }
+
+    return 0;
+
+}
+
+
+
+/* OUTPUT
+Enter 5 elements:10 20 30 40 50
+Enter element to be accessed: 40
+Element found at index: 4
+*/
