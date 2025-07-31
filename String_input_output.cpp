6e5deeb11cbd23ff4ecd73e0493969ef477c7299
Avi Sharma <avisharma5016@gmail.com>
2025-07-31 09:14:16 +0530

    String_input_output.cpp

---
+++ String_input_output.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    cout<<"Entered string: "<< str<< endl;
+    
+    return 0;
+}
+
+
+/* OUTPUT
+Enter a string: Avi Sharma
+Entered string: Avi Sharma
+*/
